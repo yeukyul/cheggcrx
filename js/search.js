67b9a9dd5bc28e28797fb6b36c5c6991731c5e29
Tiffany Lee <lyeukyu@gmail.com>
2017-07-11 23:07:43 -0700

    updated styles and enabled live search

--- js/search.js
+++ js/search.js
@@ -20,8 +20,47 @@ $(function() {
         getWikipediaExtract(search_term);
         event.preventDefault();
     });
+    
+    /* opensearch */
+    $("#search-keyword").keyup(function() {
+        var search_term = $('#search-keyword').val();
+        console.log("keyup! ", search_term);
+        $.ajax({
+            url: 'https://en.wikipedia.org/w/api.php?action=opensearch&limit=5&format=json&search=' + search_term,
+            type: 'GET',
+            dataType: "json",
+            success: function(data) {
+                console.log("got response", data[1]);
+                displayLiveSearchResult(search_term, data[1]);
+            },
+            error: function(e) {
+                console.log("doesn't work");
+                console.log(e);
+            }
+        });
+    });
         
 });
+
+function displayLiveSearchResult(search_term, suggestion) {
+    
+    if (!suggestion && !search_term) {
+        $('.search-result').html('');
+        $('.link-to-chegg').show();
+    } else if (!suggestion) {
+        showFailureResult(search_term);
+    } else {
+        var display_list = "<ul>";
+        for (var i = 0; i < suggestion.length; i++) {
+            display_list = display_list + "<li>" + suggestion[i] + "</li>";
+        }
+        display_list = display_list + "</ui>";
+        console.log(display_list);
+        $('.search-result').html(display_list);
+        $('.link-to-chegg').hide();
+    }
+    
+}
 
 
 function getWikipediaExtract(search_term) {
@@ -31,13 +70,11 @@ function getWikipediaExtract(search_term) {
                 + search_term,
          type : 'GET',
          success: function(data){
-            console.log(data);
             var page_id = Object.keys(data['query']['pages'])[0];
             var page = data['query']['pages'][page_id];
             var extract = page['extract'];
             var title = page['title'];
             var subject = "";
-            console.log(typeof(page_id));
             if (page_id === '-1') {
                 showFailureResult(title);
             } else {
@@ -49,6 +86,9 @@ function getWikipediaExtract(search_term) {
          }
     });
 }
+
+
+
 
 // placeholder: here before front-end is ready
 function showSearchResult(title, subject, extract) {
@@ -64,5 +104,5 @@ function showFailureResult(title) {
 }
 
 function errorOccured() {
-    $('.search-result').html("Sorry, error occured. Please try again.");
+    $('.search-result').html("Sorry, an unknown error occured. Please try again.");
 }
